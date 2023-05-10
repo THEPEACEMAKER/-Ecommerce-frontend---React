e5cb06a66d38cdb949f667637870823b7a019122
Ali Saad <el.zo3by@gmail.com>
2023-05-11 02:12:20 +0300

    fix bug in carousels home page

--- src/features/home/homePage.jsx
+++ src/features/home/homePage.jsx
@@ -1,37 +1,50 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Carousel from "react-bootstrap/Carousel";
 
 import styles from "./style.module.css";
-import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
-import PopularCategories from "../Category/PopularCategories/PopularCategories";
 
 function Home() {
   return (
-    <>
     <div className={`style.body`}>
-      <MDBCarousel showControls showIndicators>
-        <MDBCarouselItem
-          className="w-100 d-block"
-          itemId={1}
-          src="https://mdbootstrap.com/img/new/slides/041.jpg"
-          alt="..."
-        />
-        <MDBCarouselItem
-          className="w-100 d-block"
-          itemId={2}
-          src="https://mdbootstrap.com/img/new/slides/042.jpg"
-          alt="..."
-        />
-        <MDBCarouselItem
-          className="w-100 d-block"
-          itemId={3}
-          src="https://mdbootstrap.com/img/new/slides/043.jpg"
-          alt="..."
-        />
-      </MDBCarousel>
+      <Carousel>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            src="https://mdbootstrap.com/img/new/slides/043.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img
+            className="d-block w-100"
+            src="https://mdbootstrap.com/img/new/slides/042.jpg"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://mdbootstrap.com/img/new/slides/041.jpg"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
-    <PopularCategories></PopularCategories>
-    </>
   );
 }
 
